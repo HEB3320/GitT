30617784d576d7e4a7fbd7b14d8bbc64c0d4c38c
Sam Smith <sam@example.com>
2021-02-18 23:37:19 -0500

    bas

--- GitTrial/src/test/java/First.java
+++ GitTrial/src/test/java/First.java
@@ -3,11 +3,6 @@ public class First {
     public static void main(String[] args) {
 
         System.out.println("Hello World!!!");
-        System.out.println("Engin Bekem 33");
-        //m4
-        System.out.println("Engin Bekem 3333");
-        System.out.println("Hayri Bekem 3320");
-        //local
-        //comment
+
     }
 }
