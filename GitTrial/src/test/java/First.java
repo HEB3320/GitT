6acf0e8b18198656b56c1dc1fa35b94481851aa0
Sam Smith <sam@example.com>
2021-02-19 11:00:50 -0500

    ggg

--- GitTrial/src/test/java/First.java
+++ GitTrial/src/test/java/First.java
@@ -3,6 +3,7 @@ public class First {
     public static void main(String[] args) {
 
         System.out.println("Hello World!Remote");
-        System.out.println("local");
+                System.out.println("localHere");
+//ll
     }
 }
