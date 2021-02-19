697f76d5564652f420ccd57a7f702dbb4f41976e
Sam Smith <sam@example.com>
2021-02-19 00:00:46 -0500

    po

--- GitTrial/src/test/java/First.java
+++ GitTrial/src/test/java/First.java
@@ -3,9 +3,7 @@ public class First {
     public static void main(String[] args) {
 
         System.out.println("Hello World!!!");
-        System.out.println("pppR");
-        //llremote
-        System.out.println("pppl");
+
 
     }
 }
