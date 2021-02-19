53acb4b8bcfbae845eba72a9c514a0120f013c50
HEB3320 <55003460+HEB3320@users.noreply.github.com>
2021-02-18 23:37:34 -0500

    Update First.java

--- GitTrial/src/test/java/First.java
+++ GitTrial/src/test/java/First.java
@@ -2,11 +2,7 @@ public class First {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello World!!!");
-        System.out.println("Engin Bekem 33");
-        //m4
-        System.out.println("Engin Bekem 3333");
-        System.out.println("Hayri Bekem");
-        //comment
+        System.out.println("Hello World!!");
+       
     }
 }
